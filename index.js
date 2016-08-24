a8f16969b7c623f11491c41a0a423024ab65f9db
Robert Fitzgerald <robfitzgerald@gmail.com>
2016-08-24 12:29:17 -0600

    forgot to export router

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
 		, router = express.Router()
 
 	router
+		.use(express.static(__dirname + '/public')) // node_modules/csci7551-course-website/
 		.get('/', function(req, res, next) {
 			res.send('<html><head><title>Rob Fitzgerald CSCI 7551 Website</title></head><body><script src="public/js/test.js"></script></body></html>')
 		})
 
+	module.export = router
 }
