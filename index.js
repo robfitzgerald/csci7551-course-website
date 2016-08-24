bc873f40f04b4872d5b1726ccd02de0ce330dbeb
Robert Fitzgerald <robfitzgerald@gmail.com>
2016-08-24 13:00:45 -0600

    fixed syntax errors

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
 	router
 		.use(express.static(__dirname + '/public')) // node_modules/csci7551-course-website/
 		.get('/', function(req, res, next) {
-			res.send('<html><head><title>Rob Fitzgerald CSCI 7551 Website</title></head><body><script src="public/js/test.js"></script></body></html>')
+			res.send('<html><head><title>Rob Fitzgerald CSCI 7551 Website</title></head><body><script src="js/test.js"></script></body></html>')
 		})
 
-	module.export = router
+	module.exports = router
 }
